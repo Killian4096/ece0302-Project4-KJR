655a163aa0ef13e209b2412215a1cbb64c9a36e5
Killian4096 <killianrush203@gmail.com>
2022-04-07 00:52:56 -0400

    More work needs to be done

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -1,5 +1,7 @@
 #include "image.h"
 #include "deque.hpp"
+#include <vector>
+#include <iostream>
 
 
 
@@ -15,26 +17,101 @@ struct ImagePos{
 };
 
 //Prototypes
-ImagePos 
+ImagePos initial(Image<Pixel>& image);
+bool goal(Image<Pixel>& image, ImagePos point);
+std::vector<ImagePos> actions(Image<Pixel>& image, ImagePos currentState);
+bool containsPosition(std::vector<ImagePos>& array, ImagePos pixel);
+ImagePos breadth_first_search(Image<Pixel>& image);
+Image<Pixel> loadImage(std::string argv);
+
+int main(int argc, char *argv[])
+{
+  //Try to run it all
+  try{
+    //Load Image
+    Image<Pixel> image = loadImage(argv[1]);
+    breadth_first_search(image);
+    writeToFile(image, argv[2]);
+  }
+  //Catch one of the many errors
+  catch(const std::exception & ex){
+    std::cout << ex.what() << std::endl;
+    //Return failure
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
 
 
-bool breadth_first_search(Image<Pixel>& image){
+
+ImagePos breadth_first_search(Image<Pixel>& image){
   //Find start pixels
   ImagePos start = initial(image);
 
   //Create frontier and explored arrays
   Deque<ImagePos> frontier;
+  frontier.pushBack(start);
   std::vector<ImagePos> explored;
 
   while(true){
-    //Chec that frontier is not empty
-    if(frontier.isEmpty()){return }
+    //Check that frontier is not empty
+    if(frontier.isEmpty()){
+      throw "No Solution Found";
+    }
+
+    //Pop top from frontier
+    ImagePos s = frontier.front();
+    frontier.popFront();
+
+    //Add s to explored
+    explored.push_back(s);
+
+    //Get list of next actions
+    std::vector<ImagePos> next_actions = actions(image, s);
+
+    //Iterate over each next action
+    for(intmax_t i=0;i<next_actions.size();i++){
+      //Check if not in explored
+      if(!(containsPosition(explored, next_actions[i]))){
+        //if goal, we found it
+        if(goal(image, next_actions[i])){
+          return next_actions[i];
+        }
+        //insert into frontier
+        frontier.pushBack(next_actions[i]);
+      }
+    }
+    
   }
+
+
+
 }
 
-/*//Function to load image from file
-Image<Pixel> loadImage(char *argv[]){
-  Image<Pixel> image = readFromFile(argv[1]);
+//Function to load image from file
+Image<Pixel> loadImage(std::string argv){
+  //Read image and throw if error
+  Image<Pixel> image;
+  try{
+    image = readFromFile(argv);
+  }catch(...){
+    throw "Error";
+  }
+
+  //If any pixel other than the 3 standard colors throw
+  for(intmax_t i=0;i<image.width();i++){
+    for(intmax_t j=0;j<image.height();j++){
+      if(!(
+        image(i,j)==MAZE_AIR ||
+        image(i,j)==MAZE_WALL ||
+        image(i,j)==MAZE_START
+      )){
+        throw "Error";
+      }
+    }
+  }
+
+
   return image;
 }
 
@@ -54,16 +131,56 @@ ImagePos initial(Image<Pixel>& image){
   }
   //If the number of starts invalid, create null return
   if(starts.size()!=1){
-    //Throw exception
+    throw "Error";
   }
-  r.p = starts.at(0);
-  r.s = Yes;
-  return r;
-}*/
+  return starts[1];
+}
+
+//Returns true if we found the goal
+bool goal(Image<Pixel>& image, ImagePos point){
+  //Check if on border
+  if(
+    (point.x == 0               & point.y == 0               ) ||
+    (point.x == image.width()-1 & point.y == 0               ) ||
+    (point.x == 0               & point.y == image.height()-1) ||
+    (point.x == image.width()-1 & point.y == image.height()-1)
+  ){
+    //Draw green square
+    image(point.x, point.y) = MAZE_END;
+    //True if on border
+    return true;
+  }
+  //False if not on border
+  return false;
+}
+
+std::vector<ImagePos> actions(Image<Pixel>& image, ImagePos currentState){
+  //Create vector to hold possible steps
+  std::vector<ImagePos> steps;
+
+  //For each step, determine if valid and add to steps
+  for(unsigned short int i=0;i<2;i++){
+    for(unsigned short int j=0;j<2;j++){
+      //Make next step
+      ImagePos nextstep;
+      nextstep.x = currentState.x + i;
+      nextstep.y = currentState.y + j;
+      //Determine if valid
+      if(image(nextstep.x, nextstep.y)==MAZE_AIR){
+        steps.push_back(nextstep);
+      }
+    }
+  }
+  return steps;
+}
 
 
-
-int main(int argc, char *argv[])
-{
-  // TODO
+//Function to check if item in vector
+bool containsPosition(std::vector<ImagePos>& array, ImagePos pixel){
+  for(std::size_t i=0;i<array.size();i++){
+    if(array[i].x==pixel.x && array[i].y==pixel.y){
+      return true;
+    }
+  }
+  return false;
 }
